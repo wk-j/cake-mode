9f00a37664cf33b34b25afb5fc01e146d3c377dd
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-04-17 00:30:55 +0700

    Test assoc-default

--- cake-mode-tests.el
+++ cake-mode-tests.el
@@ -2,6 +2,7 @@
 (require 'subr-x)
 (require 'json)
 (require 'url)
+(require 'cl)
 
 ;; find ~/.emacs.d -name "f.el"
 ;; emacs -batch -l ert -l cake-mode-tests.el -f ert-run-tests-batch-and-exit
@@ -17,13 +18,61 @@
 (load-file (find-path "f.el"))
 (load-file (find-path "request.el"))
 
-(defun go ()
-  (print "Hello, world"))
+;; tests
 
-(add-hook 'emacs-startup-hook 'go)
+(defun wk-get-data (key data)
+  (assoc-default key data))
 
-(defun find--b () 
-  (find-name-dired "./" "build.cake"))
+(defun wk-query-httpbin (handler)
+  (request
+   "http://httpbin.org/get"
+   :params '(("key" . "value") ("key2" . "value2"))
+   :parser 'json-read
+
+   ;;:success (function* 'handler)
+   ;;:success 'handler
+   :success(function*
+             (lambda (&key data &allow-other-keys)
+             (message "-- I send: %s" (wk-get-data 'args data)) ))
+   
+   ;; :success (function*
+   ;;           (lambda (&key data &allow-other-keys)
+   ;;             (progn
+   ;;               (message "I sent: %S" (assoc-default 'args data))
+   ;;               (callfun handler "Data"))))))
+   ))
+
+(cl-defun wk-query-handler (&key data &allow-other-keys)
+  (progn
+    (message " -- I sent: %S" (assoc-default 'args data))))
+
+(cl-defun wk-keyword-func (&key
+                           (b 20)
+                           (a 10))
+  (progn
+    (message "%d %d" a b)))
+
+(defun wk-show-message (info)
+  (message info))
+
+;; interactive
+
+(defun run-keyword-func()
+  (interactive)
+  (wk-keyword-func
+   :b 200
+   :a 100))
+
+(defun run-show-message ()
+  (interactive)
+  (wk-show-message "Hello"))
+
+(defun run-http-handler ()
+  (interactive)
+  (wk-query-httpbin 'wk-query-handler))
+
+
+;; ert functions
 
 (ert-deftest should-get-google ()
   (request
@@ -33,23 +82,8 @@
                (progn
                  (print "Hello, world"))))))
 
-
-(defun query-httpbin (handler)
-  (request
-   "http://httpbin.org/get"
-   :params '(("key" . "value") ("key2" . "value2"))
-   :parser 'json-read
-   :success (function* handler)))
-   ;;:success (function*
-   ;;          (lambda (&key data &allow-other-keys)
-   ;;            (message "I sent: %S" (assoc-default 'args data))))))
-
-(defun http-handler ()
-  (lambda (&key data &allow-other-keys)
-    (message "I send: %s" (assoc-default 'args data))))
-
 (ert-deftest should-httpbin ()
-  (query-httpbin http-handler))
+  (query-httpbin 'http-handler))
 
 (ert-deftest should-run-execute-multiple-statment ()
   (progn
@@ -75,9 +109,6 @@
 
 (ert-deftest should-one-eq-one ()
   (should (= 1 1)))
-
-(defun find--build-cake ()
-  (find-file "build.cake" "."))
 
 (ert-deftest should-fdfind ()
   (shell-command-to-string "mdfind --name build.cake --onlyin ./"))
